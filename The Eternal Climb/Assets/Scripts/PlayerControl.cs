236327a3741c1a3c7e9ddccc175e4f07dc7da5d2
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2021-10-06 10:16:39 +0800

    Added team info to PlayerControl.cs, changed name of script

--- The Eternal Climb/Assets/Scripts/PlayerControl.cs
+++ The Eternal Climb/Assets/Scripts/PlayerControl.cs
@@ -1,8 +1,17 @@
+// GPF104 - A2 - Arcade Game-Milestone 1 (Prototype)
+// Team Project - 'The Eternal Climb' game
+// Addison Ross - 
+// Brianna Essery - 
+// Daniel Campbell - A00032368
+// Jack Pavey - 
+// Yvette Villanueva
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterClass : MonoBehaviour
+public class PlayerControl : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
