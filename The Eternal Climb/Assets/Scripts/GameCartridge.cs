067724d7ac880da04f6531ffdc070cb8b0952bb4
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2021-10-13 09:25:21 +0800

    "updated gameCartridge. Added countown timer script"
    Game cartridge script contains variables for sounds/music now also

--- The Eternal Climb/Assets/Scripts/GameCartridge.cs
+++ The Eternal Climb/Assets/Scripts/GameCartridge.cs
@@ -5,11 +5,9 @@ using UnityEngine.UI;
 
 public class GameCartridge : MonoBehaviour
 {
-    public const float REMAINING_TIME = 60.0f; // Max amount of time available to play,
-                                                // doesn't change as can't get more than max 
-                                                // time from power ups
-    public float gameTimeRemaining = 60.0f; // The current time remaining (will countdown)
+    public CountdownTimer countdownTimer;
 
+    public const float MAX_TIME = 60.0f; // Max amount of time available to play, doesn't change as can't get more than max time by killing enemies
     public AudioSource gameMusic; // Game music
     public AudioSource gameOverSound; // Game sound for game over
     public AudioSource hitSound; // Sounds for character
@@ -18,7 +16,6 @@ public class GameCartridge : MonoBehaviour
     public AudioSource enemySound;
 
     public Text scoreText;
-    public Text timerText;
 
     public int currentScore = 0; // Current score for this round
     public int highScore = 0; // The highest score acheived on this PC
@@ -30,18 +27,18 @@ public class GameCartridge : MonoBehaviour
     void Start()
     {
         isGameRunning = true;
-        scoreText.text = "Score: " + currentScore;
+        //scoreText.text = "Score: " + currentScore;
         gameMusic.Play(); // Play the scene music
-
+        countdownTimer = GameObject.Find("Timer").GetComponent<CountdownTimer>();
     }
-
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+            countdownTimer.timerIsRunning = false;
+
         if (!isGameRunning) // There migh be an easier way/better way to do this! not sure
-        {
             gameMusic.Stop();
-        }
     }
     // Function to handle adding points. Can add points when enemy is killed/collectibles etc...
     public void addPoints(int points)
