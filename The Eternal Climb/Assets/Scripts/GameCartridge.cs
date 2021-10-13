b2403eacb7b30883ac0abd137ba037dce4776b7e
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2021-10-13 08:27:35 +0800

    "updated gameCartridge script"
    Can add music, sounds, sounds, time remaining, contains score, addScore function added

--- The Eternal Climb/Assets/Scripts/GameCartridge.cs
+++ The Eternal Climb/Assets/Scripts/GameCartridge.cs
@@ -5,27 +5,23 @@ using UnityEngine.UI;
 
 public class GameCartridge : MonoBehaviour
 {
-    public const float REMAINING_TIME = 240.0f; // Max amount of time available to play,
+    public const float REMAINING_TIME = 60.0f; // Max amount of time available to play,
                                                 // doesn't change as can't get more than max 
                                                 // time from power ups
-    public float gameTimeRemaining = 240.0f; // The current time remaining (will countdown)
+    public float gameTimeRemaining = 60.0f; // The current time remaining (will countdown)
 
-    // private ObjectSpawner objectSpawner // For spawning of enemies, power ups, items? health?
-    //                                     // May be easier to just hard code where these spawn
-    // in the scene/in their own cs script
-
-    /*  public AudioSource gameMusic; // Game music
-        public AudioSource gameOverSound; // Game sound for game over
-        public AudioSource hitSound; // Sounds for character
-        public AudioSource deathSound;
-        public AudioSource powerUpSound;
-        public AudioSource enemySound;*/
+    public AudioSource gameMusic; // Game music
+    public AudioSource gameOverSound; // Game sound for game over
+    public AudioSource hitSound; // Sounds for character
+    public AudioSource deathSound;
+    public AudioSource powerUpSound;
+    public AudioSource enemySound;
 
     public Text scoreText;
     public Text timerText;
 
     public int currentScore = 0; // Current score for this round
-    //public int highScore = 0; // The highest score acheived on this PC
+    public int highScore = 0; // The highest score acheived on this PC
 
     public bool isGameRunning; // Bool is the game scene started and currently active?
 
@@ -35,8 +31,8 @@ public class GameCartridge : MonoBehaviour
     {
         isGameRunning = true;
         scoreText.text = "Score: " + currentScore;
-        //gameMusic.Play(); // Play the scene music
-        //objectSpawner = GameObject.Find("ObjectSpawner").GetComponent<ObjectSpawner>();
+        gameMusic.Play(); // Play the scene music
+
     }
 
     // Update is called once per frame
@@ -44,14 +40,13 @@ public class GameCartridge : MonoBehaviour
     {
         if (!isGameRunning) // There migh be an easier way/better way to do this! not sure
         {
-            //gameMusic.Stop();
-            //Destroy(objectSpawner); // Stop objects from spawning
+            gameMusic.Stop();
         }
     }
     // Function to handle adding points. Can add points when enemy is killed/collectibles etc...
-/*    public void  (int points)
+    public void addPoints(int points)
     {
         currentScore += points;
         scoreText.text = "Score: " + currentScore;
-    }*/
+    }
 }
