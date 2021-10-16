2d75f75914524449f14b2fc5fb8bd8e6b28cf1f6
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2021-10-16 15:14:16 +0800

    Speed power up coroutine
    Speed coroutine handles timed speed increase, returns player speed to original setting after a set duration.

--- The Eternal Climb/Assets/Scripts/PowerUpItem.cs
+++ The Eternal Climb/Assets/Scripts/PowerUpItem.cs
@@ -11,9 +11,12 @@ public class PowerUpItem : MonoBehaviour
     //TODO: add PlayerHealth reference
 
     [Header("Power Up Settings")] // Adjust these in scene for each item type
-    public float speedMultiplier;
     public float timeAdd;
     public int addHealth;
+
+    [Header("Speed Up Settings")]
+    public float speedMultiplier; // Amount to multiply speed by
+    public float duration; // How long does speed up effect last?
 
     private bool collected = false; // Added a bool as circle collider hitting multiple times in one collision
 
@@ -24,21 +27,46 @@ public class PowerUpItem : MonoBehaviour
         {
             Debug.Log(gameObject.name + "was picked up item");
             collected = true; // Set this to true to prevent multiple collisions with circle collider
-            Pickup(other);
+
+            if (speedMultiplier > 1.0f) // If the power up is a speed multipier, use IEnumerator
+                StartCoroutine(PickupSpeed(other));
+            else
+                Pickup(other);
         }
     }
 
     // Function to handle the stat changes and destroy this item
     void Pickup(Collider2D player)
     {
+        // Add power up time to the timer
+        countdownTimer.timeRemaining += timeAdd;
+
+        // Get PlayerHealth reference from other collider
+        // PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        // playerHealth.healthRemaining += addHealth; // Increase the player's health
+        
+        Debug.Log("Stats changed");
+        Destroy(this.gameObject); // Destroy the power up.. //TODO: add sound on collect
+    }    
+
+    // Coroutine for the speed power up to have a set duration
+    // before returning the speed back to normal
+    IEnumerator PickupSpeed(Collider2D player)
+    {
         // Adjust the player speed by power up amount
         PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
         playerMovement.moveSpeed *= speedMultiplier;
 
-        // Add power up time to the timer
-        countdownTimer.timeRemaining += timeAdd;
-        
+        // Item will be in the scene for duration of speed power up
+        // Disable the mesh and collider so item is not seen, and can collide again
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+
+        yield return new WaitForSeconds(duration); // Wait for 'duration' seconds
+
+        playerMovement.moveSpeed /= speedMultiplier; // Return speed back to original setting
+
         Debug.Log("Stats changed");
-        Destroy(this.gameObject); // Destroy the power up.. //TODO: add sound on collect
+        Destroy(this.gameObject); // Destroy the power up.. //TODO: add sound and maybe speed up music?
     }
 } 
