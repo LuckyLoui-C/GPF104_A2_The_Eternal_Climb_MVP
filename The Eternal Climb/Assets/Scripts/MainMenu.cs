530767ab53af710100ab9750b9457c2dd921d6e4
JakJak03 <88759924+JakJak03@users.noreply.github.com>
2021-10-17 16:40:40 +1000

    Worked On the Menu
    - Rearranged the menu to fit better
    - Swapped credits button and instead just placed them on the bottom of the screen
    - Added SceneManager for extremely easy handling of scenes and scene buttons

--- The Eternal Climb/Assets/Scripts/MainMenu.cs
+++ The Eternal Climb/Assets/Scripts/MainMenu.cs
@@ -1,20 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Play the next scene in the build index
-    public void PlayGame()
+    public GameObject credits;
+    public void ToggleCredits()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Load next scene
+        if(credits.activeSelf == true)
+        {
+            credits.SetActive(false);
+        }
+        else
+        {
+            credits.SetActive(true);
+        }
     }
-
-    // Exit the game application
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
 }
---
+++ The Eternal Climb/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    // Now any scene that has a button for scene changing can just access this and call LoadScene and tell it the name of the scene it wants to got to
+   public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
