bf2c3ed0c8f551de5de68d1b7da0bbf2c725eb96
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2021-10-23 13:47:56 +0800

    Removed double up of heathui script

--- The Eternal Climb/Assets/Scripts/HealthUI.cs
+++ The Eternal Climb/Assets/Scripts/HealthUI.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class HealthUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayerHealth playerHealth;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
