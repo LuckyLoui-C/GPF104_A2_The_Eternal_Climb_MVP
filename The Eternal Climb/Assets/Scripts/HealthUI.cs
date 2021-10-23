80c6c2a4604757a7ecd0804b120d910b4d893f08
LuckyLoui-C <67573202+LuckyLoui-C@users.noreply.github.com>
2021-10-23 18:14:23 +0800

    Health bar reacts
    Reacts to health changes and follows player

--- The Eternal Climb/Assets/Scripts/HealthUI.cs
+++ The Eternal Climb/Assets/Scripts/HealthUI.cs
@@ -4,7 +4,40 @@ using UnityEngine;
 
 public class HealthUI : MonoBehaviour
 {
+    [Header("Component References")]
     public PlayerHealth playerHealth;
+    public Sprite fullHealth;
+    public Sprite midHealth;
+    public Sprite lowHealth;
 
 
+    [Header("Display Settings")]
+    public int healthRequired; // Amount of player health needed to display this heart
+    private float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = playerHealth.health;
+    }
+
+    private void Update()
+    {
+        currentHealth = playerHealth.health;
+        if (playerHealth.health < healthRequired)
+        {
+            GetComponent<UnityEngine.UI.Image>().color = Color.black;
+        }
+        else if (playerHealth.health == 3.0f)
+        {
+            GetComponent<UnityEngine.UI.Image>().sprite = fullHealth;
+        }
+        else if (playerHealth.health == 2.0f)
+        {
+            GetComponent<UnityEngine.UI.Image>().sprite = midHealth;
+        }
+        else if (playerHealth.health == 1.0f)
+        {
+            GetComponent<UnityEngine.UI.Image>().sprite = lowHealth;
+        }
+    }
 }
